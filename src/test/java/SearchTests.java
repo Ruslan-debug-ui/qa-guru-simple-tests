373bb3fabf8f081b3099d3321addb1feffbbf372
Ruslan Ibraev <Ruslan.Ibraev@mediascope.net>
2024-09-24 13:42:48 +0300

    first commit

---
+++ src/test/java/SearchTests.java
@@ -0,0 +1,138 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SearchTests {
+
+    public void logIn () {
+        open("https://school.qa.guru");
+        $("[name=email]").setValue("zuloo@mail.ru");
+        $("[name=password]").setValue("Ruslan12%");
+        $("[id=xdget33092_1]").click();
+    }
+
+    public void logOut () {
+        //open("https://school.qa.guru");
+      //  $("[name=email]").setValue("zuloo@mail.ru");
+      //  $("[name=password]").setValue("Ruslan12%");
+        $("a[title='Профиль'] > .menu-item-icon").click();
+        sleep(1000);
+        $(".menu-item-logout > .subitem-link").click();
+        sleep(1000);
+    }
+
+
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.holdBrowserOpen = false;
+        Configuration.pageLoadStrategy = "eager";
+//        Configuration.baseUrl = "https://demoqa.com";
+//        Configuration.browserSize = "1920x1080";
+    }
+
+    @BeforeEach
+    void eachUp() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        open();
+    }
+
+
+    @Test
+    void successfulLoginTest() {
+        open("https://school.qa.guru");
+        $("[name=email]").setValue("zuloo@mail.ru");
+        $("[name=password]").setValue("Ruslan12%");
+        $("[id=xdget33092_1]").click();
+
+        $(withText("Список тренингов")).should(Condition.exist);
+        logOut();
+
+
+        //  $("[class=page-header]").shouldHave(text("Список тренингов"));
+
+    }
+
+
+    @Test
+    void negativeWrongPasswdTest() {
+        open("https://school.qa.guru");
+        $("[name=email]").setValue("zuloo@mail.ru");
+        $("[name=password]").setValue("Ruslan12").pressEnter();
+        $(".btn-error").shouldHave(text("Неверный пароль"));
+
+    }
+
+    @Test
+    void negativeNoPasswdTest() {
+        open("https://school.qa.guru");
+        $("[name=email]").setValue("zuloo@mail.ru").pressEnter();
+    //    $("[name=password]").setValue("Ruslan12").pressEnter();
+        $(".btn-error").shouldHave(text("Не заполнено поле Пароль"));
+
+    }
+
+    @Test
+    void negativeNoLoginTest() {
+        open("https://school.qa.guru");
+        //$("[name=email]").setValue("zuloo@mail.ru").pressEnter();
+        $("[name=password]").setValue("Ruslan12").pressEnter();
+        $(".btn-error").shouldHave(text("Не заполнено поле E-Mail"));
+
+    }
+
+
+    @Test
+    void negativeWrongLoginFormateTest() {
+        open("https://school.qa.guru");
+        $("[name=email]").setValue("ффф@mail.ru");
+        $("[name=password]").setValue("Ruslan12%").pressEnter();
+        $(".btn-error").shouldHave(text("Неверный формат e-mail"));
+
+    }
+
+
+    @Test
+    void negativeWrongLoginTest() {
+        open("https://school.qa.guru");
+        $("[name=email]").setValue("aaa@mail.ru");
+        $("[name=password]").setValue("Ruslan12%").pressEnter();
+        $(".btn-error").shouldHave(text("Неверный пароль"));
+
+    }
+
+    @Test
+    void positiveSupport() {
+
+    //    boolean w = true;
+    //    assertTrue(w);
+        logIn();
+        $("a[title='Служба поддержки'] > .menu-item-icon").click();
+        $(withText("Общение")).should(Condition.exist);
+        $(withText("Новый разговор")).should(Condition.exist);
+        logOut();
+
+    }
+
+    @Test
+    void positivePurchase() {
+
+        logIn();
+        $("a[title='Покупки'] > .menu-item-icon").click();
+        $(withText("Мои покупки")).should(Condition.exist);
+        logOut();
+
+    }
+
+
+}
