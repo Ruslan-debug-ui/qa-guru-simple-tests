6a3cf73bf546aac8b11b7e665d990ef5df8f09dc
Ruslan Ibraev <Ruslan.Ibraev@mediascope.net>
2024-10-10 14:35:47 +0300

    added browser settings

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -43,14 +43,14 @@ public class SearchTests {
         Configuration.browser = "chrome";
         Configuration.browserVersion = "100.0";
         Configuration.browserSize = "1920x1080";
-        //       Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-        //DesiredCapabilities capabilities = new DesiredCapabilities();
-        //capabilities.setCapability("selenoid:options", Map.<String, Object>of(
-          //      "enableVNC", true,
-          //      "enableVideo", true
-        //));
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
 
-//        Configuration.browserCapabilities = capabilities;
+        Configuration.browserCapabilities = capabilities;
 //        Configuration.baseUrl = "https://demoqa.com";
 //        Configuration.browserSize = "1920x1080";
     }
